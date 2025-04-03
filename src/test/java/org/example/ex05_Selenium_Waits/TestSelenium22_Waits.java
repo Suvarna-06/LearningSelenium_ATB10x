8b69af557c978eb59ca41d16a172a5829dc76798
Suvarna-06 <sandhyasankesh@gmail.com>
2025-04-03 22:54:45 +0530

    March 30 2025

--- src/test/java/org/example/ex05_Selenium_Waits/TestSelenium22_Waits.java
+++ src/test/java/org/example/ex05_Selenium_Waits/TestSelenium22_Waits.java
@@ -1,5 +1,21 @@
 package org.example.ex05_Selenium_Waits;
 
+import io.qameta.allure.Description;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
 public class TestSelenium22_Waits {
+        @Description("Verify Ebay Print the prices (iMac)")
+        @Test
+    public void test_verify_print_imac_price(){
+            EdgeDriver driver = new EdgeDriver();
+            driver.get("https://app.vwo.com");
+
+            try {
+                Thread.sleep(20000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
 }
