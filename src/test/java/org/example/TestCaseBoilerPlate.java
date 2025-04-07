6965c218f9501c8f4369aee5b9ae9f74b113b4d5
Suvarna-06 <sandhyasankesh@gmail.com>
2025-04-07 13:32:38 +0530

    5th April 2025

---
+++ src/test/java/org/example/TestCaseBoilerPlate.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCaseBoilerPlate {
+   public EdgeDriver driver;
+
+    @BeforeTest
+
+    public void openBrowser(){
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("--guest");
+        driver= new EdgeDriver(edgeOptions);
+
+    }
+
+    @Test
+    public void test_actions(){
+
+
+    }
+
+    @AfterTest
+    public void closeBrowser(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
