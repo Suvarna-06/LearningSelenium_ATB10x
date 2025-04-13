7ed66ba767d26797b3bbfd9f68bdde15c567149d
Suvarna-06 <sandhyasankesh@gmail.com>
2025-04-13 19:04:03 +0530

    5th April 2025

---
+++ src/test/java/org/example/ex15_Relative_locaters/Lab43_RL_Real_Example.java
@@ -0,0 +1,58 @@
+package org.example.ex15_Relative_locaters;
+
+import org.example.TestCaseBoilerPlate;
+import org.example.ex07_WaitHelper.WaitHelpers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+
+public class Lab43_RL_Real_Example  extends TestCaseBoilerPlate {
+    @Test
+    public void test_js() {
+        driver.get("https://www.aqi.in/real-time-most-polluted-city-ranking");
+        driver.manage().window().maximize();
+
+        WebElement searchBox = driver.findElement(By.xpath("//input[@placeholder=\"Search\"]"));
+        searchBox.sendKeys("india" + Keys.ENTER);
+
+        WaitHelpers.waitJVM(5000);
+
+        List<WebElement> locations = driver.findElements(By.cssSelector("div.location-name > p"));
+        for (WebElement location : locations) {
+
+            String rank = driver.findElement(with(By.tagName("p")).toLeftOf(location)).getText();
+            String aqi = driver.findElement(with(By.tagName("span")).toRightOf(location)).getText();
+            System.out.println("| +" + rank + " | " + location.getText() + " | " + aqi + " | ");
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
